b5e863b6048f6504fbced1046aebb5ab0c6c8cad
Nicolas Vanlauwe <nicolas/vanlauwe@hotmail.com>
2023-01-23 17:49:58 +0100

    Fix d'un problème de route

--- back/app.js
+++ back/app.js
@@ -3,7 +3,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const userRoutes = require('./routes/user')
 const sauceRoutes = require('./routes/sauces')
-const schemaSauce = require('./models/Sauce');
+const path = require('path');
 
 
 //Connexion BDD
@@ -19,7 +19,6 @@ mongoose.connect(`mongodb+srv://root:root@cluster0.hrlrdfh.mongodb.net/?retryWri
 
 //Fix le pb de CORS
 const app = express();
-
 app.use(express.json());
 
 app.use((req, res, next) => {
@@ -31,6 +30,7 @@ app.use((req, res, next) => {
 
 
 app.use('/api/auth', userRoutes);
-app.use('/api/sauce', sauceRoutes)
+app.use('/api/sauces', sauceRoutes);
+app.use('/images', express.static(path.join(__dirname, 'images')));
 
 module.exports = app;
--- back/controllers/user.js
+++ back/controllers/user.js
@@ -20,12 +20,12 @@ exports.login = (req, res, next) => {
     User.findOne({ email: req.body.email })
         .then(user => {
             if (user === null) {
-                res.status(401).json({ message: 'Paire identifiant/mot de passe incorrecte' })
+                return res.status(401).json({ message: 'Paire identifiant/mot de passe incorrecte' })
             } else {
                 bcrypt.compare(req.body.password, user.password)
                     .then(valid => {
                         if (!valid) {
-                            res.status(401).json({ message: 'Paire identifiant/mot de passe incorrecte' })
+                            return res.status(401).json({ message: 'Paire identifiant/mot de passe incorrecte' })
                         } else {
                             res.status(200).json({
                                 userId: user._id,
